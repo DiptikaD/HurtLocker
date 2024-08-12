f730d77e74b9c8a3d2f8d667cc4aa177e439b13e
Devi <diptikadevi@gmail.com>
2024-08-12 09:54:56 -0400

    initial commit

---
+++ src/main/java/StoreList.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class StoreList {
+
+    public static void main(String[] args) {
+
+    }
+
+    public List items;
+
+
+
+
+}
---
+++ src/main/java/Test/HurtTest.java
@@ -0,0 +1,12 @@
+package Test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HurtTest {
+
+    @Test
+    public void testTest(){
+        Assert.assertTrue(true);
+    }
+}
